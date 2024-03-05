bc7e1eea88660d28382f84dedb04b3c428592840
Anwar <enganwar100@hotmail.com>
2024-03-05 06:18:32 +0000

    initial commit

---
+++ scooter_bike_ecommerce/scooter_bike_ecommerce/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for scooter_bike_ecommerce project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'scooter_bike_ecommerce.settings')
+
+application = get_asgi_application()
