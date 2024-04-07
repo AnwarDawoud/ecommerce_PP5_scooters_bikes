3a4ea40ab009900266c46a154b3d32eedbb52cd3
Anwar <enganwar100@hotmail.com>
2024-04-07 12:27:53 -0400

    added runtime.txt

--- scooter_bike_ecommerce/scooter_bike_ecommerce/settings.py
+++ scooter_bike_ecommerce/scooter_bike_ecommerce/settings.py
@@ -22,7 +22,7 @@ load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-# Quick-start development settings - unsuitable for production
+# Quick-start development settings 'scoo- unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
@@ -30,11 +30,11 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 # DEBUG = 'DEVELOPMENT' in os.environ
 
-
-ALLOWED_HOSTS = ['scooter-bike-ecommerce-pp5.herokuapp.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = []
+# ALLOWED_HOSTS = [ter-bike-ecommerce-pp5.herokuapp.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
