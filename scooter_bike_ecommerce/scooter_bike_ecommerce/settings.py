03e4d106011d38f3b1f489913b54c9efe5315b6e
Anwar <enganwar100@hotmail.com>
2024-04-07 16:33:45 -0400

    added runtime.txt

--- scooter_bike_ecommerce/scooter_bike_ecommerce/settings.py
+++ scooter_bike_ecommerce/scooter_bike_ecommerce/settings.py
@@ -184,6 +184,8 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
 # if 'USE_AWS' in os.environ:
 #     # Cache control
 #     AWS_S3_OBJECT_PARAMETERS = {
